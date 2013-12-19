6d15967683f0dbc79a2ce0a2d6b7a35c6c9e38d9
Jared De Blander <jared@deblander.org>
2013-12-18 23:44:20 -0500

    Mt. Gox decoding started

--- workers/worker-mtgox.php
+++ workers/worker-mtgox.php
@@ -2,13 +2,19 @@
 require_once "bootstrap-worker.php";
 
 define('MTGOX_TICKER_URL',  'http://data.mtgox.com/api/2/BTCUSD/money/ticker');
+define('MTGOX_STATUS_OK',   'success');
 define('HTTP_STATUS_OK',    200);
+
 
 $response = Requests::get(MTGOX_TICKER_URL);
 
 if($response->success && $response->status_code == HTTP_STATUS_OK){
   $json_response = $response->body;
-  print_r($json_response);
+  $decode = json_decode($json_response);
+  if($decode->result == MTGOX_STATUS_OK){
+    var_dump($decode);
+  }
+  
 }else{
   print "Bad status code";
 }
